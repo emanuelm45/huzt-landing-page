e681d7daefb25cb02ab36ae125949064045d1507
Arthur Lemos <artzlemosgr@gmail.com>
2022-04-23 16:40:26 -0300

    ...

---
+++ main.js
@@ -0,0 +1,10 @@
+const fotoDrop = document.querySelector("#fotodrop")
+const fotoHuzt = document.querySelector("#fotohuzt")
+
+fotoDrop.addEventListener("click", () => {
+    document.location.href = "/shop.html"
+})
+
+fotoHuzt.addEventListener("click", () => {
+    document.location.href = "/about.html"
+})
