722e3de9c7e20339fe1a02ec7025aace766a944e
Emanuel Marques <emanuelfelipemarques45@gmail.com>
2022-04-29 22:11:49 -0300

    About page added

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const imgProducts = document.querySelector("#img-shop")
+const imgShop = document.querySelector("#img-shop")
 const imgLookbook = document.querySelector("#img-lookbook")
 const imgHuzt = document.querySelector("#img-huzt")
 
-imgProducts.addEventListener("click", () => {
+imgShop.addEventListener("click", () => {
     location.href = "./shop.html"
 })
 
