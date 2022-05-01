1412495d88302b2d5724cda62982ca93b946e172
Emanuel Marques <emanuelfelipemarques45@gmail.com>
2022-04-30 21:41:06 -0300

    e
    Signed-off-by: Emanuel Marques <emanuelfelipemarques45@gmail.com>

--- shop.js
+++ shop.js
@@ -1,10 +1,16 @@
-const productName = document.querySelector(".product__name")
+const productName = document.querySelectorAll(".product__name")
+const productPrice = document.querySelectorAll(".price")
 const btnsBuy = document.querySelectorAll(".btn-buy")
 
 btnsBuy.forEach(btnBuy => {
     btnBuy.addEventListener("click", redirectWhatsapp)
 })
 
-function redirectWhatsapp() {
-    window.location.href = "http://wa.me/5547996864477?text=Eu+gostaria+de+comprar%3A+%0D%0A" + "*" + productName.textContent + "*"
+function redirectWhatsapp(event) {
+    if (event.target == btnsBuy[0]) {
+        window.location.href = `http://wa.me/5547996864477?text=Eu+gostaria+de+comprar%3A+%0D%0A  _${encodeURI(productName[0].innerText)}_ %0D%0A *${productPrice[0].innerText}* `
+    } 
+    else {
+        window.location.href = `http://wa.me/5547996864477?text=Eu+gostaria+de+comprar%3A+%0D%0A  _${encodeURI(productName[1].innerText)}_ %0D%0A *${productPrice[1].innerText}* `
+    }
 }
